2b63419cfe7c9b871497d9d0a2347a5cf651e0cd
Gregor71 <millergregor@gmail.com>
2018-01-12 12:20:48 -0500

    remove svgo, change to htmlparser2-without-node-native

--- src/index.js
+++ src/index.js
@@ -1,48 +1,50 @@
-import * as t from './tools'
+import * as t from "./tools";
 
 const svgson = function svgson(
-  input,
-  {
-    optimize = false,
-    svgoConfig = t.svgoDefaultConfig,
-    pathsKey = '',
-    customAttrs = null,
-    compat = false,
-    camelcase = false,
-  } = {}
+    input,
+    {
+        optimize = false,
+        pathsKey = "",
+        customAttrs = null,
+        compat = false,
+        camelcase = false
+    } = {}
 ) {
-  const optimizer = input => {
-    const sanitized = t.sanitizeInput(input)
-    return optimize ? t.optimizeSVG(sanitized, svgoConfig) : Promise.resolve(sanitized)
-  }
+    const optimizer = input => {
+        const sanitized = t.sanitizeInput(input);
+        return Promise.resolve(sanitized);
+    };
 
-  const applyFilters = input => {
-    const applyPathsKey = node =>
-      pathsKey !== '' ? t.wrapInKey(pathsKey, node) : node
-    const applyCustomAttrs = node =>
-      customAttrs ? t.addCustomAttrs(customAttrs, node) : node
-    const applyCompatMode = node => (compat ? t.compat(node) : node)
-    const applyCamelcase = node =>
-      camelcase || compat ? t.camelize(node) : node
+    const applyFilters = input => {
+        const applyPathsKey = node =>
+            pathsKey !== "" ? t.wrapInKey(pathsKey, node) : node;
+        const applyCustomAttrs = node =>
+            customAttrs ? t.addCustomAttrs(customAttrs, node) : node;
+        const applyCompatMode = node => (compat ? t.compat(node) : node);
+        const applyCamelcase = node =>
+            camelcase || compat ? t.camelize(node) : node;
 
-    const result = input
-      .map(t.removeAttrs)
-      .map(applyCompatMode)
-      .map(applyPathsKey)
-      .map(applyCustomAttrs)
-      .map(applyCamelcase)
+        const result = input
+            .map(t.removeAttrs)
+            .map(applyCompatMode)
+            .map(applyPathsKey)
+            .map(applyCustomAttrs)
+            .map(applyCamelcase);
 
-    return Promise.resolve(result)
-  }
+        return Promise.resolve(result);
+    };
 
-  const haveResult = input =>
-    input.length > 0
-      ? Promise.resolve(input)
-      : Promise.reject('No result produced')
+    const haveResult = input =>
+        input.length > 0
+            ? Promise.resolve(input)
+            : Promise.reject("No result produced");
 
-  return optimizer(input).then(t.parseInput).then(applyFilters).then(haveResult)
-}
+    return optimizer(input)
+        .then(t.parseInput)
+        .then(applyFilters)
+        .then(haveResult);
+};
 
-const processInput = input => {}
+const processInput = input => {};
 
-export default svgson
+export default svgson;
--- src/tools.js
+++ src/tools.js
@@ -1,72 +1,54 @@
-import omitDeep from 'omit-deep'
-import rename from 'deep-rename-keys'
-import clean from 'clean-deep'
-import htmlparser from 'htmlparser2'
-import serializer from 'dom-serializer'
-import svgo from 'svgo'
-
-export const svgoDefaultConfig = {
-  plugins: [
-    { removeStyleElement: true },
-    { removeViewBox: false },
-    {
-      removeAttrs: {
-        attrs: '(stroke-width|stroke-linecap|stroke-linejoin|)',
-      },
-    },
-  ],
-  multipass: true,
-}
+import omitDeep from "omit-deep";
+import rename from "deep-rename-keys";
+import clean from "clean-deep";
+import htmlparser from "htmlparser2";
+import serializer from "dom-serializer";
 
 export const parseInput = input => {
-  const parsed = htmlparser.parseDOM(input, { xmlMode: true })
-  const shouldFilter = parsed.length === 1 && parsed[0].name === 'root'
-  return Promise.resolve(shouldFilter ? parsed[0].children : parsed)
-}
+    const parsed = htmlparser.parseDOM(input, { xmlMode: true });
+    const shouldFilter = parsed.length === 1 && parsed[0].name === "root";
+    return Promise.resolve(shouldFilter ? parsed[0].children : parsed);
+};
 
 export const sanitizeInput = input => {
-  const parsed = htmlparser.parseDOM(input, { xmlMode: true })
-  const f = node => node.type === 'tag' && node.name === 'svg'
-  const filtered = parsed.filter(f)
-  return serializer(filtered)
-}
+    const parsed = htmlparser.parseDOM(input, { xmlMode: true });
+    const f = node => node.type === "tag" && node.name === "svg";
+    const filtered = parsed.filter(f);
+    return serializer(filtered);
+};
 
-const wrapInput = input => `<root>${input}</root>`
+const wrapInput = input => `<root>${input}</root>`;
 
-export const optimizeSVG = (input, config) => {
-  return new svgo(config).optimize(wrapInput(input)).then(({ data }) => data)
-}
-
-export const removeAttrs = obj => omitDeep(obj, ['next', 'prev', 'parent'])
-export const wrapInKey = (key, node) => ({ [key]: node })
+export const removeAttrs = obj => omitDeep(obj, ["next", "prev", "parent"]);
+export const wrapInKey = (key, node) => ({ [key]: node });
 export const addCustomAttrs = (attrs, node) => ({
-  ...node,
-  ...attrs,
-})
+    ...node,
+    ...attrs
+});
 
 export const compat = node => {
-  const renamed = rename(node, key => {
-    if (key === 'attribs') {
-      return 'attrs'
-    }
-    if (key === 'children') {
-      return 'childs'
-    }
-    return key
-  })
-  return omitDeep(clean(renamed), ['type'])
-}
+    const renamed = rename(node, key => {
+        if (key === "attribs") {
+            return "attrs";
+        }
+        if (key === "children") {
+            return "childs";
+        }
+        return key;
+    });
+    return omitDeep(clean(renamed), ["type"]);
+};
 
 export const camelize = node => {
-  return rename(node, key => {
-    if (!notCamelcase(key)) {
-      return toCamelCase(key)
-    }
-    return key
-  })
-}
+    return rename(node, key => {
+        if (!notCamelcase(key)) {
+            return toCamelCase(key);
+        }
+        return key;
+    });
+};
 
 export const toCamelCase = prop =>
-  prop.replace(/[-|:]([a-z])/gi, (all, letter) => letter.toUpperCase())
+    prop.replace(/[-|:]([a-z])/gi, (all, letter) => letter.toUpperCase());
 
-const notCamelcase = prop => /^(data|aria)(-\w+)/.test(prop)
+const notCamelcase = prop => /^(data|aria)(-\w+)/.test(prop);
